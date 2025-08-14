53c267b07bec61ed2654a84b556b925218ae9bfe
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-14 06:56:02 +0300

    feat: implement working evaluate logic
    the evaluate function takes the user expression and evaluates it to return a result

--- script.js
+++ script.js
@@ -41,23 +41,20 @@ function divide(...args) {
 function operate(num1, operator, num2) {
     switch (operator) {
         case "+":
-            add(num1, num2);
-            break;
+            return add(num1, num2);
         case "-":
-            subtract(num1, num2);
-            break;
+            return subtract(num1, num2);
         case "*":
-            multiply(num1, num2);
-            break;
+            return multiply(num1, num2);
         case "/":
-            divide(num1, num2);
-            break;
+            return divide(num1, num2);
         default:
             break;
     }
 }
 
 clear = document.querySelector("#clear");
+equal = document.querySelector("#equal");
 display = document.querySelector(".display");
 btns = document.querySelectorAll(".calc-button");
 
@@ -73,3 +70,33 @@ for (let i = 0; i < btns.length; i++) {
         display.textContent += btns[i].textContent;
     })
 }
+
+function evaluate(expr) {
+    let operators = ["*", "/", "+", "-"];
+
+    let i = 0;
+    while (expr.length != 1) {
+        if (operators.includes(expr[i])) {
+            let num1 = Number(expr[i - 1]);
+            let operator = expr[i];
+            let num2 = Number(expr[i + 1]);
+            let res = operate(num1, operator, num2);
+            expr.splice(i - 1, 3, res);
+            i = 0;
+        }
+        i++;
+    }
+
+    return expr[0];
+}
+
+equal.addEventListener("click", () => {
+    if (display.textContent == "")
+        return;
+
+    expr = display.textContent.split(/([+\-*/])/);
+    res = evaluate(expr);
+
+    display.textContent = res;
+});
+
